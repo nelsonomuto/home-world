c8d5054eaf9be481ba7e2c5e829c01148ef5b8d6
nelson omuto <nelsonomuto@gmail.com>
2021-06-24 14:24:21 -0400

    homeworld init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+const PAGE_COUNT = 10;
+interface Person {
+  name: string;
+}
 
 function App() {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [nextPeople, setNextPeople] = useState('');
+  const [prevPeople, setPrevPeople] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const handlePersonClick = useCallback((person) => {
+    console.log({ person });
+  }, []);
+
+  const handleLoadPeople = useCallback(async (url) => {
+    setLoading(true);
+    const {
+      data: { count, next, previous, results }
+    } = await axios.get(url);
+    console.log({ count, next, previous, results });
+    setNextPeople(next);
+    setPrevPeople(previous);
+    setCount(count);
+    setPeople(results);
+    setLoading(false);
+  }, []);
+
+  const handleOnNextClick = useCallback(() => {
+    handleLoadPeople(nextPeople);
+    setCurrentPage(currentPage + 1);
+  }, [currentPage, handleLoadPeople, nextPeople]);
+
+  const handleOnPrevClick = useCallback(async () => {
+    if (currentPage > 1) {
+      handleLoadPeople(prevPeople);
+      setCurrentPage(currentPage - 1);
+    }
+  }, [currentPage, handleLoadPeople, prevPeople]);
+
+  const getPersonIndex = useCallback(
+    (index) => {
+      let pageBuffer = PAGE_COUNT * (currentPage - 1);
+      if (currentPage > 1) {
+        pageBuffer = PAGE_COUNT * (currentPage - 1);
+      }
+      console.log({ pageBuffer, currentPage });
+      return pageBuffer + index + 1;
+    },
+    [currentPage]
+  );
+
+  useEffect(() => {
+    setTotalPages(Math.ceil(count / PAGE_COUNT));
+  }, [count]);
+
+  useEffect(() => {
+    handleLoadPeople('/api/people');
+  }, [handleLoadPeople]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='app'>
+      <h3>{count} People</h3>
+      <ul className='people'>
+        {!loading &&
+          people.map((person, index) => (
+            <li className='person' key={index}>
+              <button onClick={() => handlePersonClick(person)}>
+                {getPersonIndex(index)}&nbsp;{person.name}
+              </button>
+            </li>
+          ))}
+      </ul>
+      <div className='paging'>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Page {currentPage} of {totalPages}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button disabled={loading || !prevPeople} onClick={handleOnPrevClick}>
+          Prev
+        </button>
+        <button disabled={loading || !nextPeople} className='next' onClick={handleOnNextClick}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
